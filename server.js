7776cc81112e805ebb8a3cb1dc313f7c35904825
Makan <makan@makan.bz>
2021-11-23 20:40:13 -0500

    add express server

---
+++ server.js
@@ -0,0 +1,23 @@
+/* server.js - Express server*/
+'use strict';
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/pub')))
+
+
+app.get('/', (req, res) => {
+	res.send('There is no root route, please go to example.html')
+})
+
+// will use an 'environmental variable', process.env.PORT, for deployment.
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+})
+
